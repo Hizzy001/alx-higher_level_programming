15f3ea5fa9bf9e2d338d9331b2c92956b9a1950f
Hizzy001 <hisrealh@gmail.com>
2023-11-28 21:00:35 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
-if number > 0:
-print(f"{number} is positive")
-elif number == 0:
-print(f"{number} is zero")
-else:
-print(f"{number} is negative")
+inumber = random.randint(-10, 10)
+lastdigit = abs(number) % 10
+if number < 0:
+lastdigit = -(lastdigit)
+thestring = "Last digit of {}".format (number, lastdigit)
+if lastdigit > 5:
+print(f"{thestring} and is greater than 5e")
+elif lastdigit == 0:
+print(f"{thestring} and is zero")
+elif lastdigit <6:
+print(f"{thestring} and is less than 6 and not zero")
 
