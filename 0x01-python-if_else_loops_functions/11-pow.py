9b363e9526120018b9b1a244c76f68c28af0661e
Hizzy001 <hisrealh@gmail.com>
2023-11-30 10:00:37 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-if number < 0:
-last_num = (-number % 10)
-elif number >= 0:
-last_num = number % 10
-print("{:d}".format(last_num), end="")
-return last_num
+def pow(a, b):
+    return a ** b
